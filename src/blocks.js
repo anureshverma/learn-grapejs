0116eed43f1e407393b3b680db33b309e4720886
anureshverma <anureshverma@gmail.com>
2021-04-18 17:23:53 +0530

    blocks basic

--- src/blocks.js
+++ src/blocks.js
@@ -6,4 +6,35 @@ export default (editor, opts = {}) => {
     content: { type: 'MY-COMPONENT' },
     // media: '<svg>...</svg>',
   });
-}
+
+  bm.add('section', {
+    label: '<b>Section</b>',
+    attributes: { class: 'gjs-block-section' },
+    content: `<section>
+          <h1>This is a simple title</h1>
+          <div>This is just a Lorem text: Lorem ipsum dolor sit amet</div>
+        </section>`,
+  });
+
+  bm.add('text', {
+    label: '<b>Text</b>',
+    //select: true,
+    content: {
+      tagName: 'div',
+      //draggable: false,
+      attributes: { 'some-attribute': 'some-value' },
+      components: [
+        {
+          tagName: 'span',
+          content: '<b>Some static content</b>',
+        },
+        {
+          tagName: 'div',
+          // use `content` for static strings, `components` string will be parsed
+          // and transformed in Components
+          components: '<span>HTML at some point</span>',
+        },
+      ],
+    },
+  });
+};
